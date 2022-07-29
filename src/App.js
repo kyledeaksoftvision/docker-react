c56504b0166ab3f538d4a489debfd0f606867fa6
Kyle Deak <kyle.deak@polaris.com>
2022-07-29 08:37:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Heysdfaf
         </p>
         <a
           className="App-link"
